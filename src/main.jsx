3fe62bffa98089fae66863ad9dbe58f8af2598ce
Azizbek Hafizov <azizbekhafizov678@gmail.com>
2026-04-26 11:52:34 +0500

    fix NotFound import

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { LangProvider } from './context/LangContext.jsx'
 import App from './App.jsx'
-import NotFound from './components/NotFound.jsx'
+import NotFound from './components/Notfound.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
